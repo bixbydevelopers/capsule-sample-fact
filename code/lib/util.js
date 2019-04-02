c87d69bdaa8409762c45cc367f6861a15060bb5a
Roger Kibbe <rkibbe@viv.ai>
2019-04-01 18:02:30 -0700

    ready for hackathon

--- code/lib/util.js
+++ code/lib/util.js
@@ -1,17 +1,33 @@
-exports.findItems = findItems
+exports.findContent = findContent
 
-function findItems(items, searchTerm) {
-   var matches = []
-   //a lot can be improved here...
-   for (var i=0; i<items.length; i++) {
-      if (items[i].tags) {
-        for (var j=0; j<items[i].tags.length; j++) {
-          if (searchTerm.toLowerCase() == items[i].tags[j].toLowerCase()) {
-             matches.push(items[i])
-             break
-          }
+function findContent (items, searchTerm) {
+  var matches = []
+  searchTerm = searchTerm.toLowerCase();
+
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].tags) {
+      for (var j = 0; j < items[i].tags.length; j++) {
+        if (searchTerm == items[i].tags[j].toLowerCase()) {
+          matches.push(items[i])
+          break
         }
       }
-   }
-   return matches
+    }
+  }
+  return matches
 }
+
+// Javascript way using filters
+/*exports.findContentJS = findContentJS
+function findContentJS (items, searchTerm) {
+  var matches = items.filter(function (x) {
+    if (x.tags) {
+      // Filter on filter 
+      var matchTag = x.tags.filter(function (y) {
+        return y == searchTerm
+      });
+      return (matchTag != "");
+    }
+  });
+  return matches;
+}*/
