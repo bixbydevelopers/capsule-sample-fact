23286039cfbde95675a2e6c1916df2a8b9ab55cc
Roger Kibbe <32469292+rogerkibbe@users.noreply.github.com>
2019-05-29 15:48:32 -0700

    Update code/lib/getRemoteContent.js
    Co-Authored-By: Jatin Billimoria <jbillimoria@sixfivelabs.com>

--- code/lib/getRemoteContent.js
+++ code/lib/getRemoteContent.js
@@ -4,7 +4,7 @@ var http = require('http')
 
 function getDadJoke() {
   // Very simple example of calling an API for a fact/jokke
-  // Tbis is a free and open web service - please do not abuse. See https://icanhazdadjoke.com/ for more information
+  // This is a free and open web service - please do not abuse it. See https://icanhazdadjoke.com/ for more information
   // Note Bixby http API is asyncronous - no need for a promise or callback
   var response = http.getUrl('https://icanhazdadjoke.com/', {
     format: 'json',
