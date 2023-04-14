536499cbe8e49ab16f27131fb0818a51e705d073
Felipe Gusmão <f.gusmao@samsung.com>
2023-04-14 10:20:00 -0300

    JSRS Migration

--- code/GetContent.js
+++ code/GetContent.js
@@ -1,47 +1,47 @@
-const UTIL = require("./lib/util");
-const CONTENT = require("./content");
-const GET_REMOTE = require('./lib/getRemoteContent.js')
+import UTIL from './lib/util';
+import CONTENT from './content';
+import GET_REMOTE from './lib/getRemoteContent.js';
 
 // GetContent
-exports.function = function (searchTerm) {
+export default function ({ searchTerm }) {
   //You can replace with a call to a web api - make sure you map api response to content model
-  var content = CONTENT
-  var chosenContent
+  var content = CONTENT;
+  var chosenContent;
 
   // Hard coded searchTerm example to content retrieval from an API
   if (searchTerm && searchTerm.toLowerCase() == 'dad') {
     var dadJoke = GET_REMOTE.getDadJoke();
-    
+
     // Add a random dad image
     var dadImages = ['images/dad1.png', 'images/dad2.png', 'images/dad3.png'];
-    var image = dadImages[Math.floor(dadImages.length * Math.random())]
+    var image = dadImages[Math.floor(dadImages.length * Math.random())];
 
-   return {
+    return {
       text: dadJoke,
       image: {
-        url: image
-      }
-    }
+        url: image,
+      },
+    };
   } else {
-    // Get content from local content.js file 
+    // Get content from local content.js file
     // filter based on searchTerm (note that if you use a web API, then filtering can be done in the web API itself)
     if (searchTerm) {
-      content = UTIL.findContent(content, searchTerm)
+      content = UTIL.findContent(content, searchTerm);
     }
 
     //pick a random content
     if (content.length) {
-      var index = Math.floor(content.length * Math.random())
-      chosenContent = content[index]
+      var index = Math.floor(content.length * Math.random());
+      chosenContent = content[index];
     }
   }
-  
+
   // return content if exists, else null (No Result)
   if (chosenContent) {
     return {
       text: chosenContent.text,
-      image: chosenContent.image
-    }
+      image: chosenContent.image,
+    };
   } else {
     return null;
   }
--- code/lib/util.js
+++ code/lib/util.js
@@ -1,33 +1,20 @@
-exports.findContent = findContent
+var UTIL = {  
+  findContent: function(items, searchTerm) {
+    var matches = [];
+    searchTerm = searchTerm.toLowerCase();
 
-function findContent (items, searchTerm) {
-  var matches = []
-  searchTerm = searchTerm.toLowerCase();
-
-  for (var i = 0; i < items.length; i++) {
-    if (items[i].tags) {
-      for (var j = 0; j < items[i].tags.length; j++) {
-        if (searchTerm == items[i].tags[j].toLowerCase()) {
-          matches.push(items[i])
-          break
+    for (var i = 0; i < items.length; i++) {
+      if (items[i].tags) {
+        for (var j = 0; j < items[i].tags.length; j++) {
+          if (searchTerm == items[i].tags[j].toLowerCase()) {
+            matches.push(items[i]);
+            break;
+          }
         }
       }
     }
+    return matches;
   }
-  return matches
 }
 
-// Javascript way using filters
-/*exports.findContentJS = findContentJS
-function findContentJS (items, searchTerm) {
-  var matches = items.filter(function (x) {
-    if (x.tags) {
-      // Filter on filter 
-      var matchTag = x.tags.filter(function (y) {
-        return y == searchTerm
-      });
-      return (matchTag != "");
-    }
-  });
-  return matches;
-}*/
+export default UTIL
